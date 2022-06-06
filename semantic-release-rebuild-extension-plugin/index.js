86f90f139aac3e8a106b70f84ad9f085450f63d7
Thomas Wirth <thomas@wtho.de>
2022-06-06 18:15:02 +0200

    fix: make rebuild extension run in cwd

--- semantic-release-rebuild-extension-plugin/index.js
+++ semantic-release-rebuild-extension-plugin/index.js
@@ -1,12 +1,14 @@
 const { exec } = require('child_process');
 
 module.exports = {
-  async prepare() {
+  async prepare(pluginConfig, context) {
     const command = `npm run vscode:package -w extension`;
+    const options = { cwd: context.cwd }
     return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
+      exec(command, options, (error, stdout, stderr) => {
         if (error) {
           reject(stderr);
+          return
         }
         resolve(stdout);
       });
