3f0e047baecda718b3df58621a067d059d843aac
Thomas Wirth <thomas@wtho.de>
2022-06-06 19:05:36 +0200

    fix: remove unnecessary postinstall script

--- semantic-release-rebuild-extension-plugin/index.js
+++ semantic-release-rebuild-extension-plugin/index.js
@@ -2,8 +2,8 @@ const { exec } = require('child_process');
 
 module.exports = {
   async prepare(pluginConfig, context) {
-    const command = `npm run vscode:package`;
-    const options = { cwd: `${context.cwd}/extension` }
+    const command = `npm run vscode:package -w extension`;
+    const options = { cwd: context.cwd }
     return new Promise((resolve, reject) => {
       exec(command, options, (error, stdout, stderr) => {
         if (error) {
