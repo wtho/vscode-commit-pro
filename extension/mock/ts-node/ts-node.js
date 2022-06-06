7825974af2f1f5bf2811a9748186c1493438faa3
Thomas Wirth <thomas@wtho.de>
2022-06-06 17:30:15 +0200

    feat: make ts-node mock throw on compile call

--- extension/mock/ts-node/ts-node.js
+++ extension/mock/ts-node/ts-node.js
@@ -3,6 +3,7 @@ module.exports = {
     return {
       compile: (file, done) => {
         console.log('ts-node.js: mock.compile called');
+        throw new Error('ts-node.js: mock.compile is not implemented yet!');
       }
     }
   }
